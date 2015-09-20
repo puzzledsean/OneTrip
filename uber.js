411b663a9c0336301218a50e51e4e3d4a2a98a19
Sean Zhang <puzzledsean@gmail.com>
2015-09-20 02:39:48 -0400

    WORKING YELP API FINALLY YAY

--- uber.js
+++ uber.js
@@ -7,6 +7,120 @@ var latForUber = 42.358466;
 var longForUber = -71.096059;
 var locationData;
 
+
+$.getScript("http://oauth.googlecode.com/svn/code/javascript/oauth.js", function(){
+  $.getScript("http://oauth.googlecode.com/svn/code/javascript/sha1.js", function(){
+        var priceAsked = "get price asked from user using javascript when user clicks submit";
+        var startingLoc = "get starting loc from user using javascript when user clicks submit";
+
+        //data from uber
+        var uberCityData = "insert uber api call to get city info below";
+        var uberCity = { //based on the city of boston, should be taking data from uber api
+            "baseFare":2,
+            "perMin":.16,
+            "perMile":1.24,
+            "minFare":5,
+            "safeRides":1
+        };
+        //for calculating the miles of possible travel
+        var maxRideTime = 40; //(in minutes, could be randomized or input by user)
+        var milesPossible = (priceAsked - uberCity.baseFare - maxRideTime*uberCity.perMin - uberCity.safeRides) / uberCity.perMile; 
+
+
+        var auth = {
+            //
+            // Update with your auth tokens.
+            //
+            consumerKey : "d_TgfQDhHorz-VHX8omVHQ",
+            consumerSecret : "T58G5KZiBwtgvLg6X0f6l_QkEZM",
+            accessToken : "e0znhSQNrhw5dXWE-D9RGbznzbVyAqVj",
+            // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+            // You wouldn't actually want to expose your access token secret like this in a real application.
+            accessTokenSecret : "XhSvbPBFe-6MEUq7JQW_A1Acky4",
+            serviceProvider : {
+                signatureMethod : "HMAC-SHA1"
+            }
+        };
+
+        var near = 'Boston';
+
+        var accessor = {
+            consumerSecret : auth.consumerSecret,
+            tokenSecret : auth.accessTokenSecret
+        };
+        parameters = [];
+        parameters.push(['location', near]);
+        parameters.push(['callback', 'cb']);
+        parameters.push(['radius_filter', 10]);
+        parameters.push(['oauth_consumer_key', auth.consumerKey]);
+        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+        parameters.push(['oauth_token', auth.accessToken]);
+        parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+        var message = {
+            'action' : 'http://api.yelp.com/v2/search',
+            'method' : 'GET',
+            'parameters' : parameters
+        };
+
+        OAuth.setTimestampAndNonce(message);
+        OAuth.SignatureMethod.sign(message, accessor);
+
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+        console.log(parameterMap);
+
+        var locationData;
+        $.ajax({
+            'url' : message.action,
+            'data' : parameterMap,
+            'dataType' : 'jsonp',
+            'jsonpCallback' : 'cb',
+            'success' : function(data, textStats, XMLHttpRequest) {
+                //$(#yelpInfo).val(data);
+                //$("body").append(output);
+                alert('yoaysdofjasldkfjalksdfj');
+                console.log(data);
+                locationData = data;
+
+                callback();
+            },
+            'error': function(response){
+              alert(response);
+            }
+
+        });
+
+        var hardCodeLocations = {
+            "names":
+                "Fenway"
+            ,
+            "address":
+                {"Fenway":"4 Yawkey Way, Boston, MA 02215"}
+            ,
+            //coordinates (lat and long calculated after selected?)
+            "coords":
+                {"Fenway":""}   
+        };
+
+        function callback(){
+            console.log(locationData);
+            //for calculating the random location
+            //var locations = "insert yelp/googlemaps api call to get locations within miles possible"; //(json object) 
+            var randomLoc = locationData.businesses[Math.floor((Math.random() * locationData.businesses.length) + 1)].id; //calculate the random location using hardcoded info for now (get name for now)
+            console.log(randomLoc)
+        }
+        
+        
+
+        //document.getElementById("randomLoc").innerHTML = hardCodeLocations.names;
+        //console.log(locationData);
+
+        //variables used to display info to user
+        var distanceBetweenLocs = "insert google maps api call here using startingLoc and randomLoc"; //in miles
+        var timeBetweenLocs = "insert google maps api call here using startingLoc and randomLoc"; //in minutes
+})
+})
+
 $.ajax({
   url: URL,
   headers: {
