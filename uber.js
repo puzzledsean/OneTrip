40ebc5c192d6562f27dbd78a78c9f2910474c210
bsiaotickchong <bsiaotickchong@gmail.com>
2015-09-20 01:08:59 -0400

    put yelp scripts inside uber.js
    ye

--- uber.js
+++ uber.js
@@ -5,7 +5,7 @@ var uberBaseRate, uberPerDistance;
 var endMiles = 23.24;
 var latForUber = 42.358466;
 var longForUber = -71.096059;
-
+var locationData;
 
 $.ajax({
   url: URL,
@@ -56,4 +56,62 @@ setTimeout(function (){
 //     }
 // }
 
+$.getScript("http://oauth.googlecode.com/svn/code/javascript/oauth.js", function(){
+  $.getScript("http://oauth.googlecode.com/svn/code/javascript/sha1.js", function(){
+    var auth = {
+        //
+        // Update with your auth tokens.
+        //
+        consumerKey : "cHdx9xfLppl61fZRTN8d2g",
+        consumerSecret : "7r2bfe_VvoK28JUVvdjC6zKrtZg",
+        accessToken : "4qHWBOuy_XOpSzljW9FwPVFB1DymesjO",
+        // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+        // You wouldn't actually want to expose your access token secret like this in a real application.
+        accessTokenSecret : "FfY4kSO9bKeqfD53L0QBNy5m4S8",
+        serviceProvider : {
+            signatureMethod : "HMAC-SHA1"
+        }
+    };
 
+    var near = 'San+Francisco';
+
+    var accessor = {
+        consumerSecret : auth.consumerSecret,
+        tokenSecret : auth.accessTokenSecret
+    };
+    parameters = [];
+    parameters.push(['location', near]);
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+    var message = {
+        'action' : 'http://api.yelp.com/v2/search',
+        'method' : 'GET',
+        'parameters' : parameters
+    };
+
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    //console.log(parameterMap);
+
+    $.ajax({
+        'url' : message.action,
+        'data' : parameterMap,
+        'dataType' : 'jsonp',
+        'jsonpCallback' : 'cb',
+        'success' : function(data, textStats, XMLHttpRequest) {
+            //$(#yelpInfo).val(data);
+            //$("body").append(output);
+            console.log(data);
+            locationData = data;
+        }
+    });
+  });
+
+});
+            
